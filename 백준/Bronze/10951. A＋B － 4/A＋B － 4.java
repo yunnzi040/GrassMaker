bf351a57228f90cbf97cd22bfef0e3174351afb4
yunnzi040 <zeongyunzi@gmail.com>
2025-03-14 12:38:01 +0900

    [Bronze V] Title: A+B - 4, Time: 184 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum = a+b;
+            System.out.println(sum);
+        }
+    }
+}
