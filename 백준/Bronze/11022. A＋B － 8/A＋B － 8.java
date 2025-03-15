4f5a1ab558501660d40f444cac0cf1ff87fb133e
yunnzi040 <zeongyunzi@gmail.com>
2025-03-15 21:50:29 +0900

    [Bronze V] Title: A+B - 8, Time: 228 ms, Memory: 18976 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int sum = 0;
+
+            String T = sc.nextLine();
+            int cnt = Integer.parseInt(T);
+
+            for(int i = 0; i < cnt; i++){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                sum = a + b;
+
+                System.out.println("Case #" + (i+1) + ": " + a + " + " + b + " = " + sum);
+            }
+        }
+    }
