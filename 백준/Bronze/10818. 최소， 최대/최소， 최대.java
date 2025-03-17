f38dfcde1b7ea1154ee93098414c62f583709e74
yunnzi040 <zeongyunzi@gmail.com>
2025-03-17 19:53:14 +0900

    [Bronze III] Title: 최소, 최대, Time: 1532 ms, Memory: 311460 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,4 +1,4 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
@@ -6,21 +6,17 @@ public class Main {
             //배열을 쓰지 않고 입력용 변수 하나로만 해보자
             Scanner sc = new Scanner(System.in);
             int N = sc.nextInt();
-            int[] nums = new int[N];
-
-            for (int i = 0; i < N; i++) {
-                nums[i] = sc.nextInt();
-            }
 
             int maxNum = -1000000;
             int minNum = 1000000;
 
             for (int i = 0; i < N; i++) {
-                if (nums[i] > maxNum) {
-                    maxNum = nums[i];
+                int num = sc.nextInt();
+                if (num > maxNum) {
+                    maxNum = num;
                 }
-                if (nums[i] < minNum) {
-                    minNum = nums[i];
+                if (num < minNum) {
+                    minNum = num;
                 }
             }
             System.out.println(minNum + " " + maxNum);
