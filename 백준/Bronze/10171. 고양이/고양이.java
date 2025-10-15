d5a74c86e4a6813dc62788d4b9735b1dc7d30336
Zzeong@! <zeongyunzi@gmail.com>
2025-10-15 17:35:00 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(""
+            + "\\    /\\\n"
+            + " )  ( ')\n"
+            + "(  /  )\n"
+            + " \\(__)|");
+    }
+}
