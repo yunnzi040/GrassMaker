290021ffb9785e7ce2e127daf5c4cfc216d1938e
Zzeong@! <zeongyunzi@gmail.com>
2025-10-17 15:41:24 +0900

    [Bronze V] Title: A×B, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        // 두 정수 A와 B를 입력 받음
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+        
+    }
+}
