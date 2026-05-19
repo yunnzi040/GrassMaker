e63c666f17a3d8478dc72e4555d9a045e1ff0c56
JeongYunji <zeongyunzi@gmail.com>
2026-05-20 00:47:00 +0900

    [Passed] 연속부분수열일까 (45ms, 16MB)

--- trail1/연속부분수열일까/contiguous-array-or-not.py
+++ trail1/연속부분수열일까/contiguous-array-or-not.py
@@ -1,8 +1,20 @@
 N1, N2 = map(int, input().split()) # 수열 A, B의 원소의 개수
-A = str(input())
-B = str(input())
 
-if B in A:
-    print('Yes')
-else:
-    print('No')
+# 수열 A를 입력 받습니다.
+A = list(map(str, input().split()))
+
+# 수열 B를 입력 받습니다. 
+B = list(map(str, input().split()))
+
+
+for i in range(len(A)):
+    compare = A[i:i+len(B)]
+    if compare == B:
+        vaild = "Yes"
+        break
+    else:
+        vaild = "No"
+
+print(vaild)
+
+
