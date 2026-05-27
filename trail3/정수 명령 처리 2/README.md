ef1bea34982cc192b4e7701f8fea7e538fd57326
JeongYunji <zeongyunzi@gmail.com>
2026-05-27 16:32:51 +0900

    [Passed] 정수 명령 처리 2 (153ms, 24MB)

---
+++ trail3/정수 명령 처리 2/README.md
@@ -0,0 +1,8 @@
+# [[개념]정수 명령 처리 2](https://www.codetree.ai/trails/complete/curated-cards/intro-process-numeric-commands-2)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 3 / 스택, 큐, 덱 / Queue](https://www.codetree.ai/trail-info/novice-high/) |
+| 난이도 | 쉬움 |
+| 경험치 | 10 XP |
---
+++ trail3/정수 명령 처리 2/process-numeric-commands-2.py
@@ -0,0 +1,54 @@
+N = int(input())
+command = []
+A = []
+
+for _ in range(N):
+    line = input().split()
+    command.append(line[0])
+    if line[0] == "push":
+        A.append(int(line[1]))
+    else:
+        A.append(0)
+
+# Please write your code here.
+from collections import deque
+
+class Queue:
+    def __init__(self):          # 빈 큐 하나를 생성합니다.
+        self.dq = deque()
+                
+    def push(self, item):        # 큐의 맨 뒤에 데이터를 추가합니다.
+        self.dq.append(item)
+                
+    def empty(self):             # 큐가 비어있으면 True를 반환합니다.
+        return 1 if not self.dq else 0
+                
+    def size(self):              # 큐에 들어있는 데이터 수를 반환합니다.
+        return len(self.dq)
+        
+    def pop(self):               # 큐의 맨 앞에 있는 데이터를 반환하고 제거합니다.
+        if self.empty():
+            raise Exception("Queue is empty")
+            
+        return self.dq.popleft()
+                
+    def front(self):             # 큐의 맨 앞에 있는 데이터를 제거하지 않고 반환합니다.
+        if self.empty():
+            raise Exception("Queue is empty")
+                        
+        return self.dq[0]
+
+q = Queue()
+
+for i in range(len(command)):
+    if command[i] == "push":
+        q.push(A[i])
+    elif command[i] == "front":
+        print(q.front())
+    elif command[i] == "size":
+        print(q.size())
+    elif command[i] == "empty":
+        print(q.empty())
+    elif command[i] == "pop":
+        print(q.pop())
+
